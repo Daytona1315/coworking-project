336ad1828f762c0f13e1ee9da2791e8c9407063c
Daytona1315 <155112376+Daytona1315@users.noreply.github.com>
2024-11-13 12:16:59 +0300

    First working backend build

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,1 +1,10 @@
 from fastapi import FastAPI
+
+from backend.src.business.router import router as business_router
+from backend.src.auth.router import router as auth_router
+
+
+app = FastAPI()
+
+app.include_router(business_router)
+app.include_router(auth_router)
---
+++ backend/src/database/async_engine.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from backend.src.settings import settings
+
+
+async_engine = create_async_engine(
+    settings.db_url,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    async_engine,
+    class_=AsyncSession,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+)
+
+
+async def get_async_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
